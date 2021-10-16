04c7b7ce5643f80bffd02fd9d4a792ac36f53279
GiftXXVI <gift.chimphonda@outlook.com>
2021-10-16 16:33:01 +0200

    Modified and Successfully Tested frontend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-zbyn4lak.us', // the auth0 domain prefix
+    audience: 'Udacity_Coffee_Shop', // the audience set for the auth0 app
+    clientId: 'w4CgsV1K6v7LZuMf8neldiOzzXGDdcAz', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
